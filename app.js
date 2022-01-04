5f8223f184643161c505f8b567161dd3e246f6ac
Nicholas Nolen <nicnolen@ymail.com>
2022-01-04 13:09:11 -0600

    added in name question prompt and returned user input as the value

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
-// Define the file system (fs)
-const fs = require('fs');
-// Recieve the generatePage function from the page-template function
-const generatePage = require('./src/page-template.js');
+// Use the require() function to include inquirer in the app.js file
+const inquirer = require('inquirer');
+inquirer
+  // pass questions
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?',
+    },
+  ])
+  // use the user feedback
+  .then((answers) => console.log(answers));
+// // Define the file system (fs)
+// const fs = require('fs');
+// // Recieve the generatePage function from the page-template function
+// const generatePage = require('./src/page-template.js');
 
-// Create a variable that cant be reassigned (const) and make a new array (slice) from the 3rd element (2nd index) up to but not including argv.length (up to the last array)
-const profileDataArgs = process.argv.slice(2);
+// // Variable to store the generatePage function with arguments
+// const pageHTML = generatePage(name, github);
 
-// Extract the command line arguments and store them in distinct variables using assignment destrucuring
-const [name, github] = profileDataArgs;
+// // Create an HTML file. In write file the arguments are ('file name', data, callbackFunction)
+// fs.writeFile('./index.html', pageHTML, (err) => {
+//   if (err) throw new Error(err);
 
-// Create an HTML file. In write file the arguments are ('file name', data, callbackFunction)
-fs.writeFile('./index.html', generatePage(name, github), err => {
-  if (err) throw new Error(err);
-
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
