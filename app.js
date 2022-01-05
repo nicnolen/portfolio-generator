2ad1d152fe7a57a5716113413eb75036769ef552
Nicholas Nolen <nicnolen@ymail.com>
2022-01-05 11:14:13 -0600

    added mock text and organized portfolio section into a project, about, and header section

--- app.js
+++ app.js
@@ -148,16 +148,64 @@ const promptProject = (portfolioData) => {
     });
 };
 
-// use the user feedback
-promptUser()
-  .then(promptProject)
-  .then((portfolioData) => {
-    const pageHTML = generatePage(portfolioData);
+// mock data (FOR TESTING)
+const mockData = {
+  name: 'Lernantino',
+  github: 'lernantino',
+  confirmAbout: true,
+  about:
+    'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et.',
+  projects: [
+    {
+      name: 'Run Buddy',
+      description:
+        'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.',
+      languages: ['HTML', 'CSS'],
+      link: 'https://github.com/lernantino/run-buddy',
+      feature: true,
+      confirmAddProject: true
+    },
+    {
+      name: 'Taskinator',
+      description:
+        'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.',
+      languages: ['JavaScript', 'HTML', 'CSS'],
+      link: 'https://github.com/lernantino/taskinator',
+      feature: true,
+      confirmAddProject: true
+    },
+    {
+      name: 'Taskmaster Pro',
+      description:
+        'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.',
+      languages: ['JavaScript', 'jQuery', 'CSS', 'HTML', 'Bootstrap'],
+      link: 'https://github.com/lernantino/taskmaster-pro',
+      feature: false,
+      confirmAddProject: true
+    },
+    {
+      name: 'Robot Gladiators',
+      description:
+        'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque.',
+      languages: ['JavaScript'],
+      link: 'https://github.com/lernantino/robot-gladiators',
+      feature: false,
+      confirmAddProject: false
+    }
+  ]
+};
+const pageHTML = generatePage(mockData);
 
-    // fs.writeFile('./index.html', pageHTML, err => {
-    //   if (err) throw new Error(err);
-    // });
-  });
+// use the user feedback
+// promptUser()
+//   .then(promptProject)
+//   .then((portfolioData) => {
+//     const pageHTML = generatePage(portfolioData);
+
+//     // fs.writeFile('./index.html', pageHTML, err => {
+//     //   if (err) throw new Error(err);
+//     // });
+//   });
 
 // // Variable to store the generatePage function with arguments
 // const pageHTML = generatePage(name, github);
--- src/page-template.js
+++ src/page-template.js
@@ -1,7 +1,9 @@
 // Use template literals to insert JavaScript expressions into a string through interpoliation (substitution of text for a variable we build into the html ($(variable)))
 module.exports = (templateData) => {
-  console.log(templateData);
-
+  // destructure projects and about data from templateData based on their property key names
+  // take the rest of the data (...) and store it in a new object called header
+  const { projects, about, ...header } = templateData;
+  console.log(projects, about, header);
   return `
       <!DOCTYPE html> 
       <html lang="en"> 
