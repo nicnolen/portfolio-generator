a2430b5e794b64e67784d0090ff745005a65fb23
Nicholas Nolen <nicnolen@ymail.com>
2022-01-05 11:56:52 -0600

    wrote the header, projects, and about section in HTML through node

--- src/page-template.js
+++ src/page-template.js
@@ -7,17 +7,39 @@ module.exports = (templateData) => {
   return `
       <!DOCTYPE html> 
       <html lang="en"> 
+
       <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>Portfolio Demo</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
+        <linl rel="stylesheet" href="style.css">
       </head>
     
       <body>
-        <h1>${templateData.name}</h1>
-        <h2><a href="https://github.com/${templateData.github}">Github</a></h2>
-      </body>
-      </html>
-      `;
+    <header>
+      <div class="container flex-row justify-space-between align-center py-3">
+        <h1 class="page-title text-secondary bg-dark py-2 px-3">${
+          header.name
+        }</h1>
+        <nav class="flex-row">
+          <a class="ml-2 my-1 px-2 py-1 bg-secondary text-dark" href="https://github.com/${
+            header.github
+          }">GitHub</a>
+        </nav>
+      </div>
+    </header>
+    <main class="container my-5">
+
+    </main>
+    <footer class="container text-center py-3">
+      <h3 class="text-dark">&copy; ${new Date().getFullYear()} by ${
+    header.name
+  }</h3>
+    </footer>
+  </body>
+  </html>
+  `;
 };
