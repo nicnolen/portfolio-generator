890ad680999e75c268e10045d3a33d0f547a78c6
Nicholas Nolen <nicnolen@ymail.com>
2022-01-05 12:36:17 -0600

    added in stylesheets and printed the projects data to HTML

--- src/page-template.js
+++ src/page-template.js
@@ -10,6 +10,36 @@ const generateAbout = aboutText => {
     </section>
   `;
 };
+
+// Create projects section
+const generateProjects = projectsArr => {
+  // take the array of project data and create a new array from it
+  const projectHtmlArr = projectsArr.map(
+    ({ name, description, languages, link }) => {
+      return `
+        <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
+          <h3 class="portfolio-item-title text-light">${name}</h3>
+          <h5 class="portfolio-languages">
+            Built With:
+            ${languages.join(', ')}
+          </h5>
+          <p>${description}</p>
+          <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
+        </div>
+      `;
+    }
+  );
+
+  return `
+    <section class="my-3" id="portfolio">
+      <h2 class="text-dark bg-primary p-2 display-inline-block">Work</h2>
+      <div class="flex-row justify-space-between">
+      ${projectHtmlArr.join('')}
+      </div>
+    </section>
+  `;
+};
+
 // Use template literals to insert JavaScript expressions into a string through interpoliation (substitution of text for a variable we build into the html ($(variable)))
 module.exports = templateData => {
   // destructure projects and about data from templateData based on their property key names
@@ -45,6 +75,7 @@ module.exports = templateData => {
     </header>
     <main class="container my-5">
       ${generateAbout(about)}
+      ${generateProjects(projects)}
     </main>
     <footer class="container text-center py-3">
       <h3 class="text-dark">&copy; ${new Date().getFullYear()} by ${
